c884139e113e4e25c45a89e8930da019db2a3ca2
jianbao <jianbao@yahoo-inc.com>
2014-03-23 16:02:49 -0700

    solved Remove Duplicates from Sorted Array I && II: #1

--- src/test/java/leetcode/RemoveDuplicatesFromSortedArrayII.java
+++ src/test/java/leetcode/RemoveDuplicatesFromSortedArrayII.java
@@ -13,7 +13,23 @@ Your function should return length = 5, and A is now [1,1,2,2,3].
 
 public class RemoveDuplicatesFromSortedArrayII {
     public int removeDuplicates(int[] A) {
-        return 0;
+        int MAX_REPEAT = 2;
+        if( A.length <= MAX_REPEAT ) return A.length;
 
+        int p = 0;
+        int r = 1;
+
+        for( int i = 1; i < A.length; i++ ) {
+            if( A[i] != A[p] ) {
+                A[++p] = A[i];
+                r = 1;
+            } else {
+                if( r < MAX_REPEAT ) {
+                    A[++p] = A[i];
+                    r++;
+                }
+            }
+        }
+        return p + 1;
     }
 }
