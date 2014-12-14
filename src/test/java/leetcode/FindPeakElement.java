c772e385ee4a5f9a8b887de2eb6bd22908e3152e
Jim Tao <jianbao@yahoo-inc.com>
2014-12-14 12:13:59 -0800

    find peak element; #1

---
+++ src/test/java/leetcode/FindPeakElement.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class FindPeakElement {
+    public int findPeakElement(int[] num) {
+        if (num.length == 1) {
+            return 0;
+        }
+        if (num.length == 2) {
+            return num[0] < num[1] ? 1 : 0;
+        }
+        for (int i = 1; i < num.length - 1; i++) {
+            if (num[i] > num[i-1] && num[i] > num[i+1]) {
+                return i;
+            }
+        }
+        return num[0] < num[num.length - 1] ? num.length - 1 : 0;
+    }
+
+}
