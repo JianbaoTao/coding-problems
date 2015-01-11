446d301fa00cc8c8622429a6c7d1959ed4fa7502
Jim Tao <jianbao@yahoo-inc.com>
2015-01-10 22:42:28 -0800

    MinimumPathSum; #1

---
+++ src/test/java/leetcode/MinimumPathSum.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+public class MinimumPathSum {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] P = new int[m][n]; // stores dynamic min path sum
+
+        // initialize P
+        P[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            P[i][0] = P[i-1][0] + grid[i][0];
+        }
+        for (int i = 1; i < n; i++) {
+            P[0][i] = P[0][i-1] + grid[0][i];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                int up = P[i-1][j] + grid[i][j];
+                int left = P[i][j-1] + grid[i][j];
+                P[i][j] = Math.min(up, left);
+            }
+        }
+        return P[m-1][n-1];
+    }
+}
